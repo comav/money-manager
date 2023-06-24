78dce40692db99017e4411d2652311a3a5f53371
comav <tsyomav@gmail.com>
2023-06-25 02:30:09 +0300

    Created bottom tab navigation

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import HomeScreen from "./screens/HomeScreen";
+import StatsScreen from "./screens/StatsScreen";
+import AccountsScreen from "./screens/AccountsScreen";
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName={"Home"}>
+        <Tab.Screen
+          name={"Home"}
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"home"} color={color} size={26}/>),
+          }}/>
+        <Tab.Screen
+          name={"Stats"}
+          component={StatsScreen}
+          options={{
+            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"poll"} color={color} size={26}/>),
+          }}/>
+        <Tab.Screen
+          name={"Accounts"}
+          component={AccountsScreen}
+          options={{
+            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"credit-card"} color={color} size={26}/>),
+          }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
