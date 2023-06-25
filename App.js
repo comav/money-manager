c142b81f55fd5804c4d843b513494fd1000b84b5
comav <tsyomav@gmail.com>
2023-06-25 03:24:35 +0300

    Added basic header

--- App.js
+++ App.js
@@ -4,37 +4,52 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+//Todo replace google fonts dev with respective fonts modules
+import * as Font from 'expo-font';
+import { useFonts, Poppins_400Regular, Poppins_700Bold} from "@expo-google-fonts/dev";
+
 import HomeScreen from "./screens/HomeScreen";
 import StatsScreen from "./screens/StatsScreen";
 import AccountsScreen from "./screens/AccountsScreen";
+import AppLoading from "expo-app-loading";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName={"Home"}>
-        <Tab.Screen
-          name={"Home"}
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"home"} color={color} size={26}/>),
-          }}/>
-        <Tab.Screen
-          name={"Stats"}
-          component={StatsScreen}
-          options={{
-            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"poll"} color={color} size={26}/>),
-          }}/>
-        <Tab.Screen
-          name={"Accounts"}
-          component={AccountsScreen}
-          options={{
-            tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"credit-card"} color={color} size={26}/>),
-          }}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName={"Home"}>
+          <Tab.Screen
+            name={"Home"}
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"home"} color={color} size={26}/>),
+            }}/>
+          <Tab.Screen
+            name={"Stats"}
+            component={StatsScreen}
+            options={{
+              tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"poll"} color={color} size={26}/>),
+            }}/>
+          <Tab.Screen
+            name={"Accounts"}
+            component={AccountsScreen}
+            options={{
+              tabBarIcon: ({color}) => (<MaterialCommunityIcons name={"credit-card"} color={color} size={26}/>),
+            }}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
